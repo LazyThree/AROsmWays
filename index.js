7a883822e84649c496f95af735fd39e052cbf7a3
LazyThree <45515034+LazyThree@users.noreply.github.com>
2023-01-10 15:47:19 +0800

    Update index.js

--- index.js
+++ index.js
@@ -3,6 +3,6 @@ window.onload = () => {
     const osmElement = document.querySelector('[osm]');
     document.getElementById('status').innerHTML = 'Loading OpenStreetMap data...';
     osmElement.addEventListener('vector-ways-loaded', e => {
-        document.getElementById('status').innerHTML = '';
+        document.getElementById('status').innerHTML = '成功加载了道路矢量数据';
     });
 };
