50a3eac1eee219ba46870c9eb0bceec493325de4
LazyThree <45515034+LazyThree@users.noreply.github.com>
2023-01-15 01:57:23 +0800

    Update click.js

--- click.js
+++ click.js
@@ -1,9 +1,20 @@
-window.onload = () =>{
+
+
+//实现点击事件的js代码
+window.onload = ()=>{
     document.getElementById("go").addEventListener("click", e=> {
     const lat = document.getElementById('lat').value;
     const lon = document.getElementById('lon').value;
     const minacc = document.getElementById('minacc').value;
     const el = document.querySelector("[gps-new-camera]");
-    el.setAttribute('gps-new-camera', { simulateLatitude: lat, simulateLongitude: lon, positionMinAccuracy: minacc } );
+    let index = 0;
+    setInterval(function(){    
+        let curlat = lat + 0.001*index;
+        let curlon = lon + 0.001*index;
+        el.setAttribute('gps-new-camera', { simulateLatitude: lat, simulateLongitude: lon, positionMinAccuracy: minacc } );
+        index+=1;
+    },1000);
+
     });
 }
+
