0d8e17ea93a16be2bd5e4653a49c0a3196347362
LazyThree <45515034+LazyThree@users.noreply.github.com>
2023-01-15 02:02:45 +0800

    Update click.js

--- click.js
+++ click.js
@@ -7,16 +7,7 @@ window.onload = ()=>{
     const lon = document.getElementById('lon').value;
     const minacc = document.getElementById('minacc').value;
     const el = document.querySelector("[gps-new-camera]");
-    let index = 0;
-    setInterval(function(){    
-        let curlat = lat + 0.001*index;
-        let curlon = lon + 0.001*index;
-        el.setAttribute('gps-new-camera', { simulateLatitude: lat, simulateLongitude: lon, positionMinAccuracy: minacc } );
-        console.log("当前纬度值为"+String(lat));
-        console.log("当前经度值为"+String(lon));
-        index+=1;
-    },1000);
-
+    el.setAttribute('gps-new-camera', { simulateLatitude: lat, simulateLongitude: lon, positionMinAccuracy: minacc } );
     });
 }
 
